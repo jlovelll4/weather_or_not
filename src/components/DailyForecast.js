4d322c4f9e90a2f7e4bd14bf3cb066614d62890c
jlovelll4 <jameslutherlovell@gmail.com>
2022-01-28 09:34:31 -0600

    now utilizing map function to display daily detail items.

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,61 +1,36 @@
 import React from "react";
-import { withWeather } from "../context/WeatherProvider";
+import {withWeather} from "../context/WeatherProvider";
 import "../style.css";
+import Skycons, {SkyconsType} from "react-skycons";
 
 function DailyForecast(props) {
-  if (props.daily) {
-    return (
-      <div className="display-container">
-        <span>
-          Time: {props.daily.data[0].time}
-          High: {props.daily.data[0].temperatureHigh}°F Low:{" "}
-          {props.daily.data[0].temperatureLow}°F
-        </span>
-        <span>
-          Icon: {props.daily.data[1].icon}
-          Time: {props.daily.data[1].time}
-          High: {props.daily.data[1].temperatureHigh}°F Low:{" "}
-          {props.daily.data[1].temperatureLow}°F
-        </span>
-
-        <span>
-          Icon: {props.daily.data[2].icon}
-          Time: {props.daily.data[2].time}
-          High: {props.daily.data[2].temperatureHigh}°F Low:{" "}
-          {props.daily.data[2].temperatureLow}°F
-        </span>
-
-        <span>
-          Icon: {props.daily.data[3].icon}
-          Time: {props.daily.data[3].time}
-          High: {props.daily.data[3].temperatureHigh}°F Low:{" "}
-          {props.daily.data[3].temperatureLow}°F
-        </span>
-
-        <span>
-          Icon: {props.daily.data[4].icon}
-          Time: {props.daily.data[4].time}
-          High: {props.daily.data[4].temperatureHigh}°F Low:{" "}
-          {props.daily.data[4].temperatureLow}°F
-        </span>
-
-        <span>
-          Icon: {props.daily.data[5].icon}
-          Time: {props.daily.data[5].time}
-          High: {props.daily.data[5].temperatureHigh}°F Low:{" "}
-          {props.daily.data[5].temperatureLow}°F
-        </span>
-
-        <span>
-          Icon: {props.daily.data[6].icon}
-          Time: {props.daily.data[6].time}
-          High: {props.daily.data[6].temperatureHigh}°F Low:{" "}
-          {props.daily.data[6].temperatureLow}°F
-        </span>
-      </div>
-    );
-  } else {
-    return <h3>Daily Display</h3>;
-  }
+    if (props.daily) {
+        return (
+            <section className="daily-data-container">
+                {props.daily.data.map(data => {
+                        return (
+                            <div className="daily-data-item">
+                                    <Skycons
+                                        style={{marginTop: 100}}
+                                        color="white"
+                                        size={50}
+                                        resizeClear={false}
+                                        animate={true}
+                                        type={data.icon.replace(/-/g, "_").toUpperCase()}
+                                    />
+                                <span>{props.convertTime(data.time, "dddd")}</span>
+                                <span>H: {Math.round(data.temperatureHigh)}</span>
+                                <span>L: {Math.round(data.temperatureLow)}</span>
+                                    {/*<hr />*/}
+                            </div>
+                        )
+                    })
+                }
+            < /section>
+        );
+    } else {
+        return <h3>Daily Display</h3>;
+    }
 }
+
 export default withWeather(DailyForecast);
